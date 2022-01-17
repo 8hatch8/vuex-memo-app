845df7bc2e246d251dcb65dddd0b76765e72ba4b
8hatch8 <yutaka.pg@gmail.com>
2022-01-18 00:34:43 +0900

    Listのドラッグ&ドロップ機能の実装

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,30 +5,31 @@
     </header>
     <main>
       <p class="info-line">All: {{ totalCardCount }} tasks</p>
-      <div class="list-index">
+      <draggable :list="lists" @end="saveList" class="list-index">
         <list
           v-for="(item, index) in lists"
           :key="item.id"
           :title="item.title"
           :cards="item.cards"
           :listIndex="index"
-          @change="movingCard" />
+          @change="saveList" />
       <list-add />
-      </div>
+      </draggable>
     </main>
   </div>
 </template>
 
 <script>
+import Draggable from 'vuedraggable'
 import ListAdd from './ListAdd.vue'
 import List from './List'
 import { mapState } from 'vuex'
 export default {
-  components: { ListAdd, List },
+  components: { Draggable, ListAdd, List },
   methods: {
-    movingCard() {
+    saveList() {
       this.$store.dispatch('updateList', {lists: this.lists})
-    }
+    },
   },
   computed: {
     // オブジェクトスプレット演算子でmapStateヘルパー関数を呼び出す
